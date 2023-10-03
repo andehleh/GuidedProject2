7e51d42d6ecaa41efe78f9a8684db66c8854f011
Andy Lee <choongho98@gmail.com>
2023-10-03 13:49:59 -0400

    changes to server.js

--- app/server.js
+++ app/server.js
@@ -1,9 +1,13 @@
-const express = require( "express");
+const express = require("express");
 const app = express();
 const port = 3000;
-const characterRouter =  require("./controller");
+const { getCharacter, characterRouter } = require("./controller");
 
+app.get("/api/character", async (req, res) => {
+  const character = await getCharacter();
+  res.json(character);
+});
 
-app.use("/api/planets", characterRouter);
+app.use("/api/characters", characterRouter);
 
 app.listen(port);
