5d9392bf92b8a72305f2d6da9124cb16e6a1b176
Andy Lee <choongho98@gmail.com>
2023-10-03 13:58:48 -0400

    added characters to server.js

--- app/server.js
+++ app/server.js
@@ -3,11 +3,6 @@ const app = express();
 const port = 3000;
 const characterRouter = require("./routes");
 
-app.get("/api/character", async (req, res) => {
-  const character = await getCharacter();
-  res.json(character);
-});
-
 app.use("/api/characters", characterRouter);
 
 app.listen(port);
